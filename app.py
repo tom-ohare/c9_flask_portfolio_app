193927424d8aa8136bfe084e610da3744be33ad1
tom-ohare <tom.b.ohare@gmail.com>
2020-02-16 15:51:01 +0100

    Add multiply_numbers

--- app.py
+++ app.py
@@ -35,6 +35,23 @@ def add_numbers_post():
   	      	return "Easy now! Let's keep it simple! 2 numbers with a space between them please"
 
 
+@app.route('/multiply_numbers', methods=['GET','POST'])
+def multiply_numbers_post():
+	  # --> ['5', '6', '8']
+	  # print(type(request.form['text']))
+	  if request.method == 'GET':
+	  	return render_template('multiply_numbers.html')
+	  elif request.method == 'POST':
+  	      print(request.form['text'].split())
+  	      total = 1
+  	      try:
+  	      	for str_num in request.form['text'].split():
+  	      		total *= int(str_num)
+  	      	return render_template('multiply_numbers.html', result=str(total))
+  	      except ValueError:
+  	      	return "Easy now! Let's keep it simple! 2 numbers with a space between them please"
+
+
 @app.route('/shopping_list', methods=['GET','POST'])
 def shopping_list_post():
 	  # --> ['5', '6', '8']
